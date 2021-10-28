2ad2fe2efb2c00a6752bccee6ad32fbbbacccccd
Otacilio <otaciliogsantos@gmail.com>
2021-10-28 16:17:27 -0300

    Adicionando api na pagina login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,6 +2,7 @@ import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import api from '../../Service/api'
 
 import './styles.css'
 
@@ -12,14 +13,14 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [passwordEnter, setPasswordEnter] = useState('');
 
-    const getUsersAPI = async (id: number) => {
-        const usersList = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-            .then(users => users.json())
-            .then(user => user)
+    const getUsersAPI = async () => {
+        const user = {
+            user: emailEnter,
+            senha: passwordEnter
+        }
+        const {data} = await api.post('/funcionarios/logar', user);
 
-        setEmail(usersList.email);
-
-        return;
+        console.log(data)
     }
 
     const notify = () => {
@@ -30,10 +31,7 @@ const Login = () => {
         // alert('vazou')
         event.preventDefault(); 
 
-        
-        await getUsersAPI(1);
-        
-        
+        await getUsersAPI();
         
         if (email !== emailEnter) {
             // router.push('/admin');
@@ -51,7 +49,7 @@ const Login = () => {
                 <div className="contentForm">
                     <div className="content">
                         <label htmlFor="email">E-mail</label>
-                        <input type="email" id="email" name="email" onChange={event => setEmailEnter(event.target.value)} required />
+                        <input type="text" id="email" name="email" onChange={event => setEmailEnter(event.target.value)} required />
                         <label htmlFor="name">Senha</label>
                         <input type="password" id="password" onChange={event => setPasswordEnter(event.target.value)} required />
                     </div>
--- src/pages/contexts/TableContext.tsx
+++ src/pages/contexts/TableContext.tsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import mock from '../../components/CardsMesas/mockInitial.json'
+import api from '../../Service/api';
 
 interface NewItemCount {
     produtos: [{
@@ -18,6 +19,13 @@ const ContextTable = createContext<IProduct>({} as IProduct);
 
 export const ProviderTable = ({ children }: any) => {
     const [qtdProductsCount, setQtdProductsCount] = useState(mock)
+    
+    useEffect(() => {
+        const tokenJSON = localStorage.getItem('token');
+        const token = tokenJSON ? JSON.parse(tokenJSON) : '';
+        api.defaults.headers = {Authorization: token}
+    },[])
+
     return (
         <ContextTable.Provider value={{ qtdProductsCount, setQtdProductsCount }}>
             {children}
