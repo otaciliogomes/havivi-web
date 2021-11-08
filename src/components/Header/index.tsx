68b5265b3fcadac12eeb65375877e3bfdd4f939e
M-A-Saldanha <marco.antonio.saldanha@hotmail.com>
2021-11-08 18:44:25 -0300

    Alterado css do header e do crud

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,61 +13,63 @@ const Header = ({ title }: IHeaderProps) => {
 
     return (
         <div>
-            <div className="containerHeader">
-                <h1 className="titleHeader">{title}</h1>
+            <header>
+                <div className="containerHeader menu">
+                    <h1 className="titleHeader">{title}</h1>
+                            {/* <div className="searchInput">
+                        <input className="inputSearchHeader" type="text" name="" />
+                        <BsSearch className="iconSearch" size={28} />
+                    </div> */}
+                    {!!(title != "Havivis") && (
+                        <nav className="naviContent">
+                            <button
+                                onClick={() => router.push('/admin')}
+                                className="buttonNav"
+                            >
+                                Admin
+                            </button>
+                            <button
+                                className="buttonNav"
+                                onClick={() => router.push('/produtos')}
+                            >
+                                Produtos
+                            </button>
+                            <button
+                                className="buttonNav"
+                                onClick={() => router.push('/mesas')}
+                            >
+                                Pedidos
+                            </button>
+                            <button
+                                className="buttonNav"
+                                onClick={() => router.push('/funcionarios')}
+                            >
+                                Funcionarios
+                            </button>
+                            <button
+                                className="buttonNav"
+                                onClick={() => router.push('/clientes')}
+                            >
+                                Clientes
+                            </button>
+                            <button
+                                className="buttonNav"
+                                onClick={() => router.push('/')}
+                            >
+                                Home
+                            </button>
+                        </nav>
+                    )}
+                    <AiOutlineUser
+                        size={60}
+                        className="iconLogin"
+                        onClick={() => router.push('/login')}
+                    />
+                </div>
 
-                {/* <div className="searchInput">
-                <input className="inputSearchHeader" type="text" name="" />
-                <BsSearch className="iconSearch" size={28} />
-            </div> */}
-                <AiOutlineUser
-                    size={60}
-                    className="iconLogin"
-                    onClick={() => router.push('/login')}
-                />
-            </div>
-            {!!(title != "Havivis") && (
-                <nav className="naviContent">
-                    <button
-                        onClick={() => router.push('/admin')}
-                        className="buttonNav"
-                    >
-                        Admin
-                    </button>
-                    <button
-                        className="buttonNav"
-                        onClick={() => router.push('/produtos')}
-                    >
-                        Produtos
-                    </button>
-                    <button
-                        className="buttonNav"
-                        onClick={() => router.push('/mesas')}
-                    >
-                        Mesas
-                    </button>
-                    <button
-                        className="buttonNav"
-                        onClick={() => router.push('/funcionarios')}
-                    >
-                        Funcionarios
-                    </button>
-                    <button
-                        className="buttonNav"
-                        onClick={() => router.push('/clientes')}
-                    >
-                        Clientes
-                    </button>
-                    <button
-                        className="buttonNav"
-                        onClick={() => router.push('/')}
-                    >
-                        Home
-                    </button>
-                </nav>
-            )}
-
+            </header>
         </div>
+
     )
 }
 
